f8a022f9efc96b6a19aa66aa9dba4f7070cba64e
tearcode89 <125036576+tearcode89@users.noreply.github.com>
2023-06-26 20:31:04 +0900

    33.board-edit,new

---
+++ class/pages/section09/09-01-board-new/index.js
@@ -0,0 +1,9 @@
+export default function BoardNewPage(){
+    return(
+        <>
+            <h1>등록하기</h1>
+            제목: <input type="text"/><br/>
+            내용: <input type="text"/><br/>
+            <button>등록하기</button>
+        </>)
+}
