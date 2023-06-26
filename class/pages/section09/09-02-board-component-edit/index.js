df5a27eb96e367b7aa9fcb3ecfc660cda5de3c38
tearcode89 <125036576+tearcode89@users.noreply.github.com>
2023-06-26 20:51:59 +0900

    34.board-component-edit,new

---
+++ class/pages/section09/09-02-board-component-edit/index.js
@@ -0,0 +1,9 @@
+import BoardComponent from "../../../src/components/commons/units/board/09-board-component";
+
+export default function BoardEditPage(){
+    return(
+        <>
+            <BoardComponent isEdit={true}/>
+        </>
+    )
+}
---
+++ class/pages/section09/09-02-board-component-new/index.js
@@ -0,0 +1,8 @@
+import BoardComponent from "../../../src/components/commons/units/board/09-board-component";
+
+export default function BoardNewPage(){
+    return(
+        <>
+            <BoardComponent isEdit={false}/>
+        </>)
+}
