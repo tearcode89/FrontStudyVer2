139c48ccceeaacee8296df53798eb31a781413e8
tearcode89 <sjun8932.3@gmail.com>
2023-07-06 08:37:15 +0900

    78.ORM과 DB연동

--- 03.Class_Backend/index.ts
+++ 03.Class_Backend/index.ts
@@ -1,3 +1,25 @@
-const qqq: string = '안녕하세요~~';
+// const qqq: string = '안녕하세요~~';
+//
+// console.log(qqq);
 
-console.log(qqq);
+import { DataSource } from 'typeorm'
+import {Board} from "./Board.postgres";
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "sangjun",
+    password: "335130",
+    database: "postgres",
+    entities: [Board],
+    synchronize: true,
+    logging: true,
+})
+
+AppDataSource.initialize().then(() => {
+    console.log('DB 접속에 성공하셨습니다.')
+}).catch((error) => {
+    console.log('DB 접속에 실패하였습니다.')
+    console.log('원인', error)
+})
