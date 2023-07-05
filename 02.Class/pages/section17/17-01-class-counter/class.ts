7f8dca42e9f7990db4cc10824856457270d6fe57
tearcode89 <sjun8932.3@gmail.com>
2023-07-06 08:19:48 +0900

    77.Node.js

---
+++ 02.Class/pages/section17/17-01-class-counter/class.ts
@@ -0,0 +1,38 @@
+// 02.Class Date {
+//     // qqq = 3;
+//     // getFullYear(){
+//     //
+//     // }
+//     //
+//     // getMonth(){
+//     //
+//     // }
+// // }
+//
+// // const date = new Date();
+// // console.log(date.getFullYear());
+// // console.log(date.getMonth() +1);
+//
+// 02.Class Monster{
+//     constructor() {
+//     }
+//
+//     power = 50
+//
+//     attack(){console.log('공격합니다')}
+// }
+//
+// 02.Class SuperMonster extends Monster{
+//     run(){console.log('도망가자')}
+//
+//     attack(){
+//         console.log('필살기~!!')
+//     }
+// }
+// const newMonster = new Monster();
+// newMonster.power;
+// newMonster.attack();
+//
+// const myMonster = new SuperMonster()
+// myMonster.run()
+// myMonster.attack();
---
+++ 03.Class_Backend/index.ts
@@ -0,0 +1,3 @@
+const qqq: string = '안녕하세요~~';
+
+console.log(qqq);
