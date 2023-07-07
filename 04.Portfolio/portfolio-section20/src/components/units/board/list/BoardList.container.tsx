95d0338c90a111c33ba94f0d8f1b8290c8b96144
tearcode89 <sjun8932.3@gmail.com>
2023-07-07 19:58:53 +0900

    94.포트폴리오 리뷰

---
+++ 04.Portfolio/portfolio-section20/src/components/units/board/list/BoardList.container.tsx
@@ -0,0 +1,53 @@
+import BoardListUI from "./BoardList.presenter";
+import { useQuery } from "@apollo/client";
+import { FETCH_BOARDS, FETCH_BOARDS_COUNT } from "./BoardList.queries";
+import { useRouter } from "next/router";
+import type {
+  IQuery,
+  IQueryFetchBoardsArgs,
+  IQueryFetchBoardsCountArgs,
+} from "../../../../commons/types/generated/types";
+import { useState } from "react";
+import type { MouseEvent } from "react";
+
+export default function BoardList(): JSX.Element {
+  const router = useRouter();
+  const [keyword, setKeyword] = useState("");
+
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  const { data: dataBoardsCount, refetch: refetchBoardsCount } = useQuery<
+    Pick<IQuery, "fetchBoardsCount">,
+    IQueryFetchBoardsCountArgs
+  >(FETCH_BOARDS_COUNT);
+
+  const onClickMoveToBoardNew = (): void => {
+    void router.push("/boards/new");
+  };
+
+  const onClickMoveToBoardDetail = (
+    event: MouseEvent<HTMLDivElement>
+  ): void => {
+    void router.push(`/boards/${event.currentTarget.id}`);
+  };
+
+  const onChangeKeyword = (value: string): void => {
+    setKeyword(value);
+  };
+
+  return (
+    <BoardListUI
+      data={data}
+      onClickMoveToBoardNew={onClickMoveToBoardNew}
+      onClickMoveToBoardDetail={onClickMoveToBoardDetail}
+      refetch={refetch}
+      refetchBoardsCount={refetchBoardsCount}
+      count={dataBoardsCount?.fetchBoardsCount}
+      keyword={keyword}
+      onChangeKeyword={onChangeKeyword}
+    />
+  );
+}
